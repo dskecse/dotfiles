41cd189a914307226cdd6b140c39728d614b7197
Dzianis Dashkevich <dskecse@gmail.com>
2014-08-24 14:55:47 +0300

    Provide `ipcalc` utility after system bootstrapping

--- config_image.sh
+++ config_image.sh
@@ -29,6 +29,7 @@ sudo apt-get install -y graphviz
 sudo apt-get install -y tree
 sudo apt-get install -y lsb-release
 sudo apt-get install -y traceroute
+sudo apt-get install -y ipcalc
 sudo apt-get install -y htop
 sudo apt-get install -y xclip
 sudo apt-get install -y keepassx
